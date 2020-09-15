4159a75d063732b8856934ef3bac30e29b414c9a
Abubakar <muhammad_abubakar@live.com>
2020-09-15 13:26:22 +0500

    First Commit for admin-portal

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Button} from "@material-ui/core";
+/** @format */
+
+import React from "react";
+import { Button } from "@material-ui/core";
 
 function App() {
-    return (
-        <div className="App">
-            <Button>This is a Material Button</Button>
-
-        </div>
-    );
+  return (
+    <div className='App'>
+      <Button>This is a Material Button</Button>
+    </div>
+  );
 }
 
 export default App;
